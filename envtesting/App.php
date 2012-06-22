a501690338cc29121798f521698de59b452a576b
Roman Ožana <roman@wikidi.com>
2012-06-22 14:31:26 +0200

    Add license file

--- envtesting/App.php
+++ envtesting/App.php
@@ -2,9 +2,13 @@
 namespace envtesting;
 
 /**
- * Envtesting application
+ * Envtesting is fast simple and easy to use environment testing written in PHP.
+ * Can check library, services and services response.
+ *
+ * Produce console, HTML or CSV output.
  *
  * @author Roman Ozana <ozana@omdesign.cz>
+ * @license MIT
  */
 class App {
 	/** @var string */
