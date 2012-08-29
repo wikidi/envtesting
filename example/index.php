3afd816b43628c705cdb770443cea62f58e5d99b
Roman Ožana <roman@wikidi.com>
2012-08-29 11:07:36 +0200

    Fixing complex index example

--- example/index.php
+++ example/index.php
@@ -5,21 +5,28 @@ namespace envtesting;
  *
  * @author Roman Ozana <ozana@omdesign.cz>
  */
-//if (file_exists(__DIR__ . '/Envtesting.php')) require_once __DIR__ . '/Envtesting.php';
-//if (file_exists(__DIR__ . '/../Envtesting.php')) require_once __DIR__ . '/../Envtesting.php';
+require_once __DIR__ . '/../Envtesting.php'; // or
+// require_once __DIR__ . '/Envtesting.php'; // depends on your structure
 
-require_once __DIR__ . '/../envtesting/Autoloader.php';
+Throws::allErrors(); // throws everything (warnings, notices, fatals) as reular Exceptions
 
 Suite::instance('Copy & Paste example'); // change Suite name
 Suite::instance()->failGroupOnFirstError(); // fail whole group on first error
 
 // add some tests here
 
-$mysql = Suite::instance()->mysql;
+$mysql = Suite::instance()->mysql; // create new Group
 /** @var Suite $mysl */
 
 $mysql->addTest('PDO', __DIR__ . '/../envtests/library/Pdo.php', 'library');
-$mysql->addTest('PDO', __DIR__ . '/../envtests/library/Pdo.php', 'library');
 
-// output
-echo Suite::instance()->run();
+// create another group
+$apc = Suite::instance()->apc;
+/** @var Suite $apc */
+$apc->addTest('APC', __DIR__ . '/../envtests/library/Apc.php', 'library');
+
+
+echo Suite::instance()->run(); // or
+// Suite::instance()->run()->render(); // for getting HTML and CSV output
+
+Throws::nothing();
